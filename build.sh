b4a521dd433d831bce88bc615e22cfc097d4a015
Eric Allam <eallam@icloud.com>
2019-07-19 10:02:50 +0100

    Added metadata so it can be published to the SAR

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 echo '***Start***'
 
-INSTRUCTION="If you want to deploy, try running the script with 'deploy region bucket-prefix profile', e.g. 'deploy eu-west-2 solveless-stack-source-code solve-dev-eric'"
+INSTRUCTION="If you want to deploy, try running the script with 'deploy region bucket profile', e.g. 'deploy eu-west-2 solveless-stack-source-code solve-dev-eric'"
 
 if [ $# -eq 0 ]; then
   echo "Missing arguments." 
@@ -18,6 +18,13 @@ elif [ "$1" = "deploy" ] && [ $# -eq 4 ]; then
   sam build
   sam package --template-file ./.aws-sam/build/template.yaml --s3-bucket $BUCKET_NAME --output-template-file ./.aws-sam/build/packaged.yml --region $AWS_REGION --profile $PROFILE
   aws cloudformation deploy --template-file ./.aws-sam/build/packaged.yml --stack-name custom-aws-sdk-layer --capabilities CAPABILITY_IAM --region $AWS_REGION --profile $PROFILE
+elif [ "$1" = "publish" ] && [ $# -eq 3 ]; then
+  REGION=$2
+  BUCKET_NAME=$3
+  
+  sam build
+  aws cloudformation package --template-file .aws-sam/build/template.yaml --output-template-file .aws-sam/build/packaged.yml --s3-bucket $BUCKET_NAME --region $REGION
+  sam publish --template .aws-sam/build/packaged.yml --region $REGION
 else
   echo $INSTRUCTION
   exit 1
